7007aa2f432b6c8e76c6ebee9493fcd4aec5f109
Nikhil Atyali <nikhilatyali90@gmail.com>
2024-12-31 23:18:26 +0530

    added components and  routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Login from './components/Login';
+import Browse from './components/Browse';
 
 function App() {
-
+const appRouter = createBrowserRouter([
+      {
+        path : '/',
+        element : <Login />
+      },
+      {
+        path: '/browse',
+        element: <Browse/>
+      }
+])
 
   return (
     <>
-     <div className='text-red-600'>Hello  World</div>
+     <RouterProvider router={appRouter} />
     </>
   )
 }
